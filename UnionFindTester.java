d2d1e9b7da608c26cac15db8e21d10036416a69b
imachan7 <mr_ima_kino@yahoo.co.jp>
2025-06-26 02:49:06 +0900

    Unionfindのバグの修正

--- UnionFindTester.java
+++ UnionFindTester.java
@@ -17,12 +17,5 @@ class UnionFindTester{
 
 
 
-        // テストパターン5
-        UnionFind kadai3_2 = new UnionFind(10);
-        System.out.println("4を親9を子としてつなげる");
-        kadai3_2.unite(4,9);
-        System.out.println("9を親0を子としてつなげられるか" + kadai3_2.unite(9,0));
-        System.out.println("0と4はつなげられるか" + kadai3_2.unite(0,4));
-        System.out.println("0の根は" + kadai3_2.find(0));
     }
 }
