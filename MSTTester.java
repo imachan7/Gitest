43fb35b0f86ecd2eadcfde08a87441f2bc97e693
imachan7 <mr_ima_kino@yahoo.co.jp>
2025-06-26 02:38:46 +0900

    クラスカル法の時の出力の追加

--- MSTTester.java
+++ MSTTester.java
@@ -1,6 +1,7 @@
 // 課題4: クラスカル法 MSTTester.java
 class MSTTester{
     public static void main(String[] args){
+        System.out.println("クラスカル法スタート");
         WUGraph kadai4 = new WUGraph(args[0]);
         WUGraph MstGraph = kadai4.getMST();
         System.out.println("TotalWeight: "+MstGraph.getSumCost());
